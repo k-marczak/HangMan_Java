8132349f9e4c54ad10acac1c5dd3573e9552d7a6
k-marczak <k_marczak@outlook.com>
2023-08-15 14:18:19 +0200

    add catch to constructor, add load challenge func

--- src/WordDB.java
+++ src/WordDB.java
@@ -1,8 +1,11 @@
 import java.awt.datatransfer.FlavorEvent;
 import java.io.BufferedReader;
 import java.io.FileReader;
+import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
+import java.util.Random;
 
 public class WordDB {
 
@@ -21,13 +24,44 @@ public class WordDB {
             String filePath = getClass().getClassLoader().getResource(CommonConstants.DATA_PATH).getPath();
 
             if(filePath.contains("%20")) filePath = filePath.replaceAll("%20", " ");
-            BufferedReader reader = new BufferedReader(new FileReader(filePath)));
+            BufferedReader reader = new BufferedReader(new FileReader(filePath));
 
             String line;
             while((line = reader.readLine()) != null){
-                String parts = line.split(",")
+                String[] parts = line.split(",");
+
+                // the first word of each line represents the caregoeu
+
+                String category = parts[0];
+                categories.add(category);
+
+                // the rest of the values from parts be the words rea;tove to the category
+                String values[] = Arrays.copyOfRange(parts, 1, parts.length);
+                wordList.put(category, values);
+
             }
+        }catch(IOException e){
+            System.out.println("Error: " + e);
         }
     }
+
+
+    public String[] loadChallenge(){
+
+        Random rand = new Random();
+
+        // generate random number to choose category
+        String category = categories.get(rand.nextInt(categories.size()));
+
+        // generate random number to choose the value from category
+        String[] categoryValues = wordList.get(category);
+        String word = categoryValues[rand.nextInt(categoryValues.length)];
+
+
+        // [0]  -> category and [1] -> word
+        return new String[] {category.toUpperCase(), word.toUpperCase()};
+
+    }
+
 
 }
