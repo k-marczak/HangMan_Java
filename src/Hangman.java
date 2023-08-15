c8de986b917d10a24e533eba532000931a525d74
k-marczak <k_marczak@outlook.com>
2023-08-15 16:23:59 +0200

    add gui components

--- src/Hangman.java
+++ src/Hangman.java
@@ -1,6 +1,26 @@
 import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class Hangman extends JFrame {
+public class Hangman extends JFrame implements ActionListener     {
+    // counts the number of incorrect guesses player has made.
+    private int incorrectGuesses;
+
+    // store the challenge from the wordDB
+    private String[] wordChallenge;
+
+    private final WordDB wordDB;
+    private JLabel hangmanImage;
+    private JLabel categoryLabel;
+    private JLabel hiddenWordLabel;
+    private JButton[] letterButtons;
+
+
+
+
+
+
 
     public Hangman() {
         super("Hangman Game");
@@ -9,7 +29,114 @@ public class Hangman extends JFrame {
         setLocationRelativeTo(null);
         setLayout(null);
         setResizable(false);
+        getContentPane().setBackground(CommonConstants.BACKGROUND_COLOR);
+
+        // init vars
+        wordDB = new WordDB();
+        letterButtons = new JButton[26];
+        wordChallenge = wordDB.loadChallenge();
+
+
+        addGuiComp();
+
     }
 
 
+    private void addGuiComp(){
+
+        // hangman image
+        hangmanImage = CustomTools.loadImage(CommonConstants.IMAGE_PATH);
+        hangmanImage.setBounds(0, 0, hangmanImage.getPreferredSize().width, hangmanImage.getPreferredSize().height);
+
+
+        // category display
+        categoryLabel = new JLabel(wordChallenge[0]);
+        categoryLabel.setHorizontalAlignment(SwingConstants.CENTER);
+
+        categoryLabel.setOpaque(true);
+        categoryLabel.setForeground(Color.WHITE);
+        categoryLabel.setBackground(CommonConstants.SECONDARY_COLOR);
+        categoryLabel.setBorder(BorderFactory.createLineBorder(CommonConstants.SECONDARY_COLOR));
+        categoryLabel.setBounds(
+                0,
+                hangmanImage.getPreferredSize().height - 28,
+                CommonConstants.FRAME_SIZE.width,
+                categoryLabel.getPreferredSize().height);
+
+
+
+
+
+        //hidden word
+        hiddenWordLabel = new JLabel(CustomTools.hideWords(wordChallenge[1]));
+        hiddenWordLabel.setForeground(Color.white);
+        hiddenWordLabel.setHorizontalAlignment(SwingConstants.CENTER);
+
+        hiddenWordLabel.setBounds(
+                0,
+                categoryLabel.getY() + categoryLabel.getPreferredSize().height + 50,
+                CommonConstants.FRAME_SIZE.width,
+                hiddenWordLabel.getPreferredSize().height
+        );
+
+
+        // letter buttons
+        GridLayout gridLayout = new GridLayout(4, 7);
+        JPanel buttonPanel = new JPanel();
+        buttonPanel.setBounds(
+                -5,
+                hiddenWordLabel.getY() + hiddenWordLabel.getPreferredSize().height,
+                CommonConstants.BUTTON_PANEL_SIZE.width,
+                CommonConstants.BUTTON_PANEL_SIZE.height
+        );
+        buttonPanel.setLayout(gridLayout);
+
+        // create the letter buttons
+        for(char c = 'A'; c <= 'Z'; c++) {
+            JButton button = new JButton(Character.toString(c));
+            button.setBackground(CommonConstants.PRIMARY_COLOR);
+            button.setForeground(Color.white);
+            button.addActionListener(this);
+
+
+            // using ASCII values to calcualte the current index
+            int currentIndex = c - 'A';
+
+            letterButtons[currentIndex] = button;
+            buttonPanel.add(letterButtons[currentIndex]);
+        }
+
+
+        // reset Button
+        JButton resetButton = new JButton("Reset");
+        resetButton.setForeground(Color.WHITE);
+        resetButton.setBackground(CommonConstants.SECONDARY_COLOR);
+        resetButton.addActionListener(this);
+        buttonPanel.add(resetButton);
+
+
+
+
+        // Quit button
+
+        JButton quitButton = new JButton("Quit");
+        quitButton.setForeground(Color.WHITE);
+        quitButton.setBackground(CommonConstants.SECONDARY_COLOR);
+        quitButton.addActionListener(this);
+        buttonPanel.add(quitButton);
+
+
+
+        getContentPane().add(categoryLabel);
+        getContentPane().add(hangmanImage);
+        getContentPane().add(hiddenWordLabel);
+        getContentPane().add(buttonPanel);
+
+
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+    }
 }
