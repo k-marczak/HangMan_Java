1b707a202e70d5c21c7804b1bc4ff4bdd911b557
k-marczak <k_marczak@outlook.com>
2023-08-15 14:08:14 +0200

    Add Commmon constatnc class

---
+++ src/CommonConstants.java
@@ -0,0 +1,9 @@
+import java.awt.*;
+
+public class CommonConstants {
+
+    public static final String DATA_PATH = "data.txt";
+
+    public static final Dimension FRAME_SIZE = new Dimension(540, 760);
+
+}
